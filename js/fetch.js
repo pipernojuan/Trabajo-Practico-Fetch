b293c9ea0aadf3be07789600d5f643d582db3163
piperno <juan.piperno@gmail.com>
2022-07-14 03:11:01 -0300

    v3

--- js/fetch.js
+++ js/fetch.js
@@ -1,13 +1,18 @@
-
 // Ejemplo Práctico de Fetch
 
 
-let url = 'https://jsonplaceholder.typicode.com/users'
+let url = 'https://jsonplaceholder.typicode.com/users';
 fetch (url)
     .then(response => response.json())
     .then(data => mostrarData(data))
     .catch (error => console.log(error))
 
-const mostrarData = (data) =>{
+const mostrarData = (data) => {
     console.log (data)
+    let body = ''
+    for (let i = 0; i<data.length; i++) {
+        body+= `<tr><td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].email}</td></tr>`
+    }
+
+    document.getElementById ('data').innerHTML = body
 }
